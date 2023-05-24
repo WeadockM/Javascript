3d102c028425047877c562a542eb1df2e7fd37c6
Mason <mgweadock@aol.com>
2023-05-23 20:21:19 -0500

    Initial upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//////////////////////////////////////
+//                                  //
+//      Node.js JavaScipt File      //
+//                                  //
+//////////////////////////////////////
+
+// Clears console on file save //
+console.clear();
+
+// Code
+
+console.log('Do stuff')
