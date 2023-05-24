bbb9834b2ebd75496a1e719c32f20e7cdf4c4eb2
Mason <mgweadock@aol.com>
2023-05-23 21:49:38 -0500

    class progress

--- main.js
+++ main.js
@@ -11,4 +11,109 @@ console.clear();
 
 // Code
 
-console.log('This is a template.');
+// let loggedInUser;
+
+// if (loggedInUser) {
+    //     console.log('Get out of here')
+    // }
+
+
+// let flavor = 'cherry';
+// 
+// if(!(flavor === 'grape' || flavor === 'cherry')) {
+// console.log("We don't have that flavor")
+// }
+
+// let day = 4;
+// 
+// if(day === 1){
+    // console.log("Monday")
+// }
+// else if(day === 2){
+    // console.log("Tuesday")
+// }
+// else if(day === 3){
+    // console.log("Wednesday")
+// }
+// else if(day === 4){
+    // console.log("Thursday")
+// }
+// else if(day === 5){
+    // console.log("Friday")
+// }
+// else if(day === 6){
+    // console.log("Saturday")
+// }
+// else if(day === 7){
+    // console.log("Sunday")
+// }
+// else{
+    // console.log('INVALID DAY')
+// }
+
+// switch(day){
+    // case 1:
+        // console.log('Monday');
+        // break;
+    // case 2:
+        // console.log('Tuesday');
+        // break;
+    // case 3:
+        // console.log('Wednesday');
+        // break;
+    // case 4:
+        // console.log('Thursday');
+        // break;
+    // case 5:
+        // console.log('Friday');
+        // break;
+    // case 6:
+        // console.log('Saturday');
+        // break;
+    // case 7:
+        // console.log('Sunday');      
+        // break;            
+    // default:
+        // console.log('INVALID DAY')                  
+// }
+
+// let emoji = 'sad face';
+// 
+// switch (emoji) {
+// case 'happy face':
+// case 'sad face':
+    // console.log('yellow');
+    // break;
+// case 'eggplant':
+    // console.log('purple');
+    // break;
+// case 'heart':
+// case 'lips':
+    // console.log('red');
+    // break;
+// }
+
+// let num = 7;
+
+// if (num === 7) {
+    // console.log("lucky!")
+// }
+// else {
+    // console.log('BAD!')
+// }
+
+// num === 7 ? console.log('lucky!') : console.log('BAD!')
+
+// let status1 = 'offine';
+
+// let color;
+// if(status1 === 'offline') {
+//     color = 'red';
+// }
+// else{
+//     color = 'green';
+// }
+
+// let color = status1 === 'offline' ? 'red' : 'green';
+// console.log(color)
+
